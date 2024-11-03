250e12936de61e4ae35ef54963c6fbe0778a7be0
brwinner5350 <59104918+brwinner5350@users.noreply.github.com>
2024-11-02 19:49:53 -0500

    Implement MIL debug break

--- shared/util/utillib/debugbreak.h
+++ shared/util/utillib/debugbreak.h
@@ -1,3 +1,10 @@
-#include <wdf.h>
-#include <Windows.h>
-#include <Shlwapi.h>
+#ifndef C_UTILLIB_DEBUGBREAK
+#define C_UTILLIB_DEBUGBREAK
+
+#include "precomp.h"
+
+void MilUnexpectedError(HRESULT hr, LPCTSTR pszContext);
+void MilInstrumentationBreak(DWORD dwFlags, bool fDebugBreak);
+void MilDisableInstrumentationBreaks();
+
+#endif
